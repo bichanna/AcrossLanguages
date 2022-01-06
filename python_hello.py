c29b452904528b277c608d45fe75d4d89c554c1f
bichanna <nobu.bichanna@gmail.com>
2022-01-05 20:44:14 -0600

    Call R function from Python

--- python_hello.py
+++ python_hello.py
@@ -7,3 +7,10 @@ from julia import Main
 Main.eval('include("julia_hello.jl")')
 jl_hello = Main.eval("hello")
 jl_hello("bichanna")
+
+## calling a R function
+import rpy2.robjects as robjects
+r = robjects.r
+r["source"]("r_hello.R")
+r_hello = robjects.globalenv["hello"]
+r_hello("bichanna")
