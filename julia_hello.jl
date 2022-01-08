998dcc94169dca8d6c2b034ea7babc3ed41d435c
bichanna <nobu.bichanna@gmail.com>
2022-01-07 18:34:08 -0600

    Call R function from Julia

--- julia_hello.jl
+++ julia_hello.jl
@@ -7,3 +7,8 @@ using PyCall
 pushfirst!(PyVector(pyimport("sys")."path"), @__DIR__)
 pyhello = pyimport("python_hello")
 pyhello.hello("bichanna")
+
+## calling a R function
+using RCall
+R"""source("r_hello.R")"""
+R"""hello("bichanna")"""
