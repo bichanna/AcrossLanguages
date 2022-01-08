e2d028bb064d0f8837f28509019713226beb75ae
bichanna <nobu.bichanna@gmail.com>
2022-01-07 19:12:32 -0600

    Call Python function from R

--- r_hello.r
+++ r_hello.r
@@ -1,3 +1,10 @@
 r_hello <- function(name) {
 	print(paste("Hello", name, sep=" "))
 }
+
+## calling a Python function
+# install.packages("reticulate", repos="http://cran.us.r-project.org") # might be needed
+library("reticulate")
+use_python("/usr/local/bin/python3", required=T) # choose python you want
+source_python("python_hello.py")
+py_hello("bichanna")
